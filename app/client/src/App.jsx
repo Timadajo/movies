620a12232a6ed167acf6e52dfb52e3c7d1a6a418
Matheus Castro <matheus.oliveira.luna.garcia@gmail.com>
2025-06-28 17:38:14 -0300

    Eu tentei implementar o carrossel, so que ne, so tentei mesmo

--- app/client/src/App.jsx
+++ app/client/src/App.jsx
@@ -1,10 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import viteLogo from '/vite.svg';
+import './App.css';
+import  MainCarousel  from './Components/CarroselTeste.jsx';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import CustomArrows from './Components/Seta.jsx';
 
 function App() {
-  
+  return (
+    <>
+    <MainCarousel />
+    <CustomArrows />
+    </>
+  )
 }
 
 export default App
--- app/client/src/Paginas/Início.jsx
+++ app/client/src/Paginas/Início.jsx
@@ -1,3 +1,11 @@
 import "../Menu.css"
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import SimpleSlider from "../Components/CarroselTeste";
+
+
+
 
 export default Inicio;
+
